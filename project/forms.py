5dcac10a0e11ebf1c5d0b311f389e10016e98156
Anshul Agarwal <iwm2017008@iiita.ac.in>
2020-09-26 16:24:28 +0530

    Password field name updated

--- project/forms.py
+++ project/forms.py
@@ -4,14 +4,14 @@ from wtforms.validators import DataRequired
 
 
 class Edit(FlaskForm):
-    access=PasswordField('Access Code',validators=[DataRequired()])
+    access=PasswordField('Password Code',validators=[DataRequired()])
     information_block = StringField('Information Block',validators=[DataRequired()])
     attribute = StringField('Attribute', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 class Delete(FlaskForm):
-    access=PasswordField('Access Code',validators=[DataRequired()])
+    access=PasswordField('Password Code',validators=[DataRequired()])
     information_block = StringField('Information Block',validators=[DataRequired()])
     attribute = StringField('Attribute', validators=[DataRequired()])
     submit = SubmitField('Submit')
